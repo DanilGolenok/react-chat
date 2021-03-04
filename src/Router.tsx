4726d5f3147923225c3ffb6891f9127fafb44936
DanilGolenok <golenok.danil456@gmail.com>
2021-03-04 18:25:33 +0200

    get rid of react-typesafe-routes, cuz we have react-router-dom

--- src/Router.tsx
+++
@@ -1,9 +0,0 @@
-import { Router } from 'react-typesafe-routes';
-
-const Noop: React.FC = () => null;
-
-export const router = Router((route) => ({
-	home: route('/', {
-		component: Noop,
-	}),
-}));
