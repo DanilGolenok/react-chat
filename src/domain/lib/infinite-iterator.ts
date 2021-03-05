4e773dac38a004bfec4988f1c472a83c4fc006b8
DanilGolenok <golenok.danil456@gmail.com>
2021-03-05 10:38:59 +0200

    add infinite iterator

---
+++ src/domain/lib/infinite-iterator.ts
@@ -0,0 +1,13 @@
+export function* infiniteIterator<TData>(data: TData[]) {
+	if (data.length === 0) return;
+
+	let idx = 0;
+
+	while (true) {
+		if (idx === data.length - 1) {
+			idx = 0;
+		}
+
+		yield data[idx++];
+	}
+}
