57e4f60658ed40ce4623601c0034cc004ecac433
DanilGolenok <golenok.danil456@gmail.com>
2021-03-06 18:55:33 +0200

    fix: provide redux store to the root

--- src/ui/app/@core/App.tsx
+++ src/ui/app/@core/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import { ProfileProvider } from 'gateway/profile/providers/ProfileProvider';
 import { AppView } from '../views/AppView';
+import { StoreProvider } from 'store';
 
 export const App: React.FC = () => (
-	<ProfileProvider>
-		<AppView />
-	</ProfileProvider>
+	<StoreProvider>
+		<ProfileProvider>
+			<AppView />
+		</ProfileProvider>
+	</StoreProvider>
 );
