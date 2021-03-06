ee1bf0b1ee56982fa726921286f049fd5d454b1c
DanilGolenok <golenok.danil456@gmail.com>
2021-03-06 15:54:39 +0200

    app layout setup

--- src/ui/app/layouts/AppLayout/AppLayout.tsx
+++ src/ui/app/layouts/AppLayout/AppLayout.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-export const AppLayout: React.FC = ({ children }) => {
-	return <div></div>;
-};
+import { AppHeader } from '../../components/AppHeader';
+import { AppNavBar } from '../../components/AppNavBar';
+
+import 'AppLayout.scss';
+
+export const AppLayout: React.FC = ({ children }) => (
+	<div className="app-layout">
+		<div className="app-layout__sidebar">
+			<AppNavBar />
+		</div>
+		<div className="app-layout__main">
+			<AppHeader />
+			<div className="app-layout__content">{children}</div>
+		</div>
+	</div>
+);
