3ef47db7017c9b63b375dd614c7be280f45f1256
DanilGolenok <golenok.danil456@gmail.com>
2021-03-08 01:59:32 +0200

    add app routes

--- src/ui/routes/@core/Routes.tsx
+++ src/ui/routes/@core/Routes.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-import { BrowserRouter } from 'react-router-dom';
 import { PublicRoutes } from '../components/PublicRoutes';
 
-export const Routes: React.FC = () => (
-	<BrowserRouter>
-		<PublicRoutes />
-	</BrowserRouter>
-);
+export const Routes: React.FC = () => <PublicRoutes />;
