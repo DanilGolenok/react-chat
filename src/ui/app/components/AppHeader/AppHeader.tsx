b395df1f66e867f477d041d984e24cf86a7065fb
DanilGolenok <golenok.danil456@gmail.com>
2021-03-08 12:29:50 +0200

    app layout updates & add header navigation

--- src/ui/app/components/AppHeader/AppHeader.tsx
+++ src/ui/app/components/AppHeader/AppHeader.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
-export const AppHeader: React.FC = () => {
-	return <div></div>;
-};
+import { RedirectIconButton } from 'ui/@core/components/RedirectIconButton';
+import { BellIcon, ChatIcon, HomeIcon, SettingsIcon } from 'ui/assets/components';
+import { AppRoutes } from 'ui/routes/const/app-routes';
+
+import './AppHeader.scss';
+
+export const AppHeader: React.FC = () => (
+	<div className="app-header">
+		<RedirectIconButton icon={HomeIcon} link={AppRoutes.Home} />
+		<RedirectIconButton icon={ChatIcon} link={AppRoutes.Chat} />
+		<RedirectIconButton icon={BellIcon} link={AppRoutes.Notifications} />
+		<RedirectIconButton icon={SettingsIcon} link={AppRoutes.Settings} />
+	</div>
+);
